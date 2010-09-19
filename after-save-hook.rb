99cf12c24481e0fca307a19a8bebe82e745990ba
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-09-19 14:23:55 -0400

    Don't operate on current dir for new/unnamed files.  Refactor "if" blocks and stuff.

--- after-save-hook.rb
+++ after-save-hook.rb
@@ -2,29 +2,27 @@ require 'pathname'
 require 'yaml'
 
 nanoc_compile_proc = Proc.new do |buffer|
-  if buffer && buffer.name
-    path = Pathname.new( buffer.name ).dirname
-  else
-    path = '.'
-  end
+  break  if buffer.nil?
+  break  if buffer.name.nil?
 
-  if path.exist?
-    last_path = nil
-    loop do
-      config_file = Dir[ "#{path}/config.yaml" ][0]
-      if config_file
-        config = YAML::load_file( config_file )
-        if config[ 'output_dir' ]
-          output = `pushd #{path}; nanoc compile; popd >/dev/null`.split( /\n/ )[-1]
-          $diakonos.set_iline output
-          break
-        end
+  path = Pathname.new( buffer.name ).dirname
+  break  if ! path.exist?
+
+  last_path = nil
+  loop do
+    config_file = Dir[ "#{path}/config.yaml" ][0]
+    if config_file
+      config = YAML::load_file( config_file )
+      if config[ 'output_dir' ]
+        output = `pushd #{path}; nanoc compile; popd >/dev/null`.split( /\n/ )[-1]
+        $diakonos.set_iline output
+        break
       end
-
-      path = path.parent
-      break  if path == last_path
-      last_path = path
     end
+
+    path = path.parent
+    break  if path == last_path
+    last_path = path
   end
 end
 
